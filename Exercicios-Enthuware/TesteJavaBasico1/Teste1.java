8258cbfa067b1fbeb7829c4f7a7411b975c50b11
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-10-21 20:50:10 -0300

    Atualização na Elaboração de Teste de Pacotes

---
+++ Exercicios-Enthuware/TesteJavaBasico1/Teste1.java
@@ -0,0 +1,7 @@
+public class Teste1{
+	public static void main(String []args){
+		System.out.println(new A().main);
+	}
+	
+	int main; //O segredo pe a chamada da variável main pois a mesma está no escopo da classe
+}
---
+++ Exercicios-Enthuware/TesteJavaBasico1/root/com/foo/X.java
@@ -0,0 +1,7 @@
+package root.com.foo;
+public class X{
+	public static int LOGICID = 10;
+	public void apply(int i){
+		System.out.println("Applied");
+	}
+}
